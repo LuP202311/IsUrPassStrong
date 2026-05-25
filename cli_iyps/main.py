a008b1f5ea29777eb133018143800c5a154fddfd
LuP202311 <149633372+LuP202311@users.noreply.github.com>
2026-05-25 22:34:54 +0200

    Update main.py

--- cli_iyps/main.py
+++ cli_iyps/main.py
@@ -11,7 +11,7 @@ app.add_typer(passwords_app, name="passwords")
 @app.callback()
 def default(ctx: typer.Context):
     if ctx.invoked_subcommand is None:
-        typer.echo("Instructions :\n entrer dans le dossier ")
+        typer.echo("Voici les instructions pour tester le programme avec la CLI: \n1. Entrez dans le dossier `cli_iyps` (avec la commande `cd cli_iyps`)\n2. Pour vérifier si le mot de passe est robuste: `python main.py verifier mdp ""<mot_de_passe>""` ou `python main.py verifier interactif""`\n3. Pour générer un mot de passe: `python main.py generate`")
 
 if __name__ == "__main__":
     app()
